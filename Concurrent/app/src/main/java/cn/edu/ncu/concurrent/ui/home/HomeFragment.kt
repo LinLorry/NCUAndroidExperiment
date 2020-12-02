94edd487e46f9f0b5525fee67c533c06debb3861
LinLorry <linlorry.p@gmail.com>
2020-12-02 17:00:41 +0800

    :fire: Remove unusable code.

--- Concurrent/app/src/main/java/cn/edu/ncu/concurrent/ui/home/HomeFragment.kt
+++ Concurrent/app/src/main/java/cn/edu/ncu/concurrent/ui/home/HomeFragment.kt
@@ -8,21 +8,10 @@ import android.view.ViewGroup
 import cn.edu.ncu.concurrent.R
 
 class HomeFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_home, container, false)
-    }
-
-    companion object {
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-            }
     }
 }
